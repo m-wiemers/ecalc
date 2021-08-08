dddb87b4b1efb7e24b151fe3b481c7422efce40a
m-wiemers <marcel@klebauter.de>
2021-08-08 12:45:56 +0200

    feat: InputSection

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import "react-native-gesture-handler";
 import React from "react";
-import { Text, View, Dimensions } from "react-native";
+import { Text, View, Dimensions, StyleSheet } from "react-native";
+import InputSection from "./components/InputSection";
 
 const windowWidth = Dimensions.get("screen").width;
 const windowHeight = Dimensions.get("screen").height;
@@ -8,17 +9,20 @@ const windowHeight = Dimensions.get("screen").height;
 const App = () => (
   <View style={styles.container}>
     <Text style={styles.text}>Hier kommt was neues</Text>
+    <InputSection type={false} label="Label:" />
+    <InputSection type label="Label:" />
   </View>
 );
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 25,
   },
   text: {
     color: "#ff6600",
+    marginBottom: 20,
   },
-};
+});
 
 export default App;
---
+++ components/InputSection.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  TextInputProps,
+} from "react-native";
+
+const windowWidth = Dimensions.get("screen").width;
+const windowHeight = Dimensions.get("screen").height;
+
+type StyleProps = TextInputProps & {
+  type: boolean;
+  label: string;
+};
+
+const InputSection = ({ type, label, ...props }: StyleProps): JSX.Element => {
+  return (
+    <View
+      style={[styles.container, { backgroundColor: type ? "#c9c9c9" : "#fff" }]}
+    >
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={styles.input} {...props}></TextInput>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 5,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  label: {
+    maxWidth: 150,
+    paddingBottom: 2,
+  },
+  input: {
+    borderColor: "#999",
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 3,
+    width: 200,
+  },
+});
+
+export default InputSection;
